8d3401547d7e645ed557b794aaa88881778be12b
wlswo <byunjin11@naver.com>
2026-06-03 02:31:35 +0900

    feat: add Cache-Control header to static files to mitigate cold-start asset failures

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ from fastapi.staticfiles import StaticFiles
 
 from api import routes
 
+
+class CachedStaticFiles(StaticFiles):
+    """StaticFiles with a Cache-Control header so CSS/JS are served from the
+    CDN/browser cache and don't hit the (possibly sleeping) origin."""
+
+    def file_response(self, *args, **kwargs):
+        response = super().file_response(*args, **kwargs)
+        response.headers["Cache-Control"] = "public, max-age=86400"
+        return response
+
+
 app = FastAPI()
 BASE_DIR = Path(__file__).resolve().parent
-app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")),
+app.mount("/static", CachedStaticFiles(directory=str(BASE_DIR / "static")),
           name="static")
 
 app.include_router(routes.router)
